0e30eaeae932616eee031387862cb8dea447cc32
Tarsbir Singh <tarsbirsingh@gmail.com>
2018-05-15 23:09:03 +0530

    changes

--- app/src/main/java/com/example/hp/miwok/MainActivity.java
+++ app/src/main/java/com/example/hp/miwok/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hp.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void openNumberList(View view) {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
 }
