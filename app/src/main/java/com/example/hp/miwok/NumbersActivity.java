b73fa8e67b3e3ca525d963522741686c3f19e819
Tarsbir Singh <tarsbirsingh@gmail.com>
2018-05-20 21:05:58 +0530

    Change Array With ArrayList

--- app/src/main/java/com/example/hp/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/hp/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.hp.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,16 +13,19 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
         //create array of words
-        String[] words = new String[10];
-        words[0]= "one";
-        words[1]= "two";
-        words[2]= "three";
-        words[3]= "four";
-        words[4]= "five";
-        words[5]= "six";
-        words[6]= "seven";
-        words[7]= "eight";
-        words[8]= "nine";
-        words[9]= "ten";
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.v("NumbersActivity","Words at index 0" + words.get(0));
+        Log.v("NumbersActivity", "Words at index 5"+words.get(5));
     }
 }
